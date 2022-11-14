4c35ed2041d3c7cbf4838217648ddd9bb78eb314
Luca Pagano <lu.pagano@gmail.com>
2022-11-14 10:27:10 +0100

    wigner1 removed

---
+++ alternative_wigner/d1mach__genmod.f90
@@ -0,0 +1,11 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Nov 11 10:53:28 2022
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE D1MACH__genmod
+          INTERFACE 
+            FUNCTION D1MACH(I)
+              INTEGER(KIND=4) :: I
+              REAL(KIND=8) :: D1MACH
+            END FUNCTION D1MACH
+          END INTERFACE 
+        END MODULE D1MACH__genmod
---
+++ alternative_wigner/i1mcry__genmod.f90
@@ -0,0 +1,14 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Nov 11 10:53:28 2022
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE I1MCRY__genmod
+          INTERFACE 
+            SUBROUTINE I1MCRY(A,A1,B,C,D)
+              INTEGER(KIND=4) :: A
+              INTEGER(KIND=4) :: A1
+              INTEGER(KIND=4) :: B
+              INTEGER(KIND=4) :: C
+              INTEGER(KIND=4) :: D
+            END SUBROUTINE I1MCRY
+          END INTERFACE 
+        END MODULE I1MCRY__genmod
